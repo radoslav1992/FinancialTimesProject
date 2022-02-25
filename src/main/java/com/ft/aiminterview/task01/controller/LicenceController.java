50f7395f4eec546ce940415ae4eb19be16ba5e76
Radoslav Dodnikov <radoslav.dodnikov@taryafintech.com>
2022-02-25 17:01:43 +0200

    Fixed allocate seat mapping.

--- src/main/java/com/ft/aiminterview/task01/controller/LicenceController.java
+++ src/main/java/com/ft/aiminterview/task01/controller/LicenceController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +41,8 @@ public class LicenceController {
     }
     //TODO add http endpoint for amending LicenseStatus
     // do not implement DELETE endpoint
+    @PutMapping
+
 
     @GetMapping
     public ResponseEntity<List<LicenseResponseDto>> listAllLicenses() {
@@ -55,7 +58,7 @@ public class LicenceController {
 
     //TODO implement validation for `joinedDate` >= Now() and  `seatExpiryDate` >= `joinedDate`,
     // return HTTP 422 'Unprocessable Entity' in validation can't pass
-    @PostMapping("/{licenceId}/allocate-seat")
+    @PostMapping("/allocate-seat/{licenceId}")
     public ResponseEntity<?> allocateSeat(
             @PathVariable("licenceId") ObjectId licenceId,
             @RequestBody Seat seat) {
